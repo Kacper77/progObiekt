5394ab07d4efd8e65471cb20db72f28162ea29cb
Kacper Stroisz <kacperstroisz@wp.pl>
2018-02-15 15:22:56 +0100

    Change .js

--- js/script.js
+++ js/script.js
@@ -5,12 +5,22 @@ function Phone(brand, price, color,system,RAM) {
 	this.system = system;
 	this.RAM = RAM;
 }
-Phone.prototype.printInfo = function() {
+/*Phone.prototype.printInfo = function() {
 	console.log("The phone brand is " + this.brand + ", color is " + this.color + ", the price is " + this.price + ", the system is " + this.system + " and the phone contains memory " + this.RAM + ".");
 }
-var iPhone6S = new Phone("Apple", 2250, "silver", "iOS", "32GB");
-	iPhone6S.printInfo();
-var SamsungGalaxyS6 = new Phone("Samsung", 1600, "white", "Android", "32GB");
-	SamsungGalaxyS6.printInfo();
-var OnePlusOne = new Phone("OnePlus", 1400, "black", "Android", "16GB");
-	OnePlusOne.printInfo();
+	var iPhone6S = new Phone("Apple", 2250, "silver", "iOS", "32GB");
+		iPhone6S.printInfo();
+	var SamsungGalaxyS6 = new Phone("Samsung", 1600, "white", "Android", "32GB");
+		SamsungGalaxyS6.printInfo();
+	var OnePlusOne = new Phone("OnePlus", 1400, "black", "Android", "16GB");
+		OnePlusOne.printInfo(); */
+
+Phone.prototype.printInfo = function() {
+	console.log(`The phone brand is ${this.brand}, color is ${this.color}, price is ${this.price}, the system is ${this.system}, and the phone contains memory ${this.RAM}.`);
+}
+	var iPhone6S = new Phone("Apple", 2250, "silver", "iOS", "32GB");
+		iPhone6S.printInfo();
+	var SamsungGalaxyS6 = new Phone("Samsung", 1600, "white", "Android", "32GB");
+		SamsungGalaxyS6.printInfo();
+	var OnePlusOne = new Phone("OnePlus", 1400, "black", "Android", "16GB");
+		OnePlusOne.printInfo();
